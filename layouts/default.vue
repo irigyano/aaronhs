969737ac6c863704b8e03b46bfe3e3a0ecc3ab60
irigyano <anhsieh10665@gmail.com>
2023-06-18 15:55:07 +0800

    chore: init Nuxt 3 layouts, pages and Tailwind

---
+++ layouts/default.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="bg-red-500">Layout</div>
+  <slot />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+    consectetur architecto. Quos quod, optio nulla quis necessitatibus, ullam
+    sint autem id inventore accusantium est ratione sed voluptas eos pariatur
+    accusamus.
+  </div>
+</template>
