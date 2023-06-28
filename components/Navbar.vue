4a70ea12ddd677297287649a0f100c3b05bb2444
irigyano <anhsieh10665@gmail.com>
2023-06-28 18:20:23 +0800

    style: navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,27 @@
 <template>
-  <nav class="flex justify-around items-center border border-red-500">
-    <NuxtLink to="/" class="text-xl text-cyan-300">irigyano</NuxtLink>
-    <NuxtLink to="/categories">Categories</NuxtLink>
-    <button>Moon</button>
+  <nav
+    class="flex items-center bg-white rounded py-1 pl-6 pr-11 border border-zinc-300 relative"
+  >
+    <NuxtLink
+      to="/"
+      class="border border-zinc-300 flex justify-center items-center basis-1/12 p-1"
+      v-for="btn in 12"
+    >
+      <img
+        class="w-full h-full object-cover"
+        src="https://irowiki.org/w/images/6/6e/Double_Strafe.png?20070923200807"
+      />
+    </NuxtLink>
+    <!-- <NuxtLink to="/categories">Categories</NuxtLink> -->
+    <button
+      class="absolute m-1 top-0 right-0 h-3 w-3 rounded-full bg-gradient-to-b border-[0.25px] border-zinc-300 from-white via-blue-700 to-white"
+      @click="showAlert"
+    ></button>
   </nav>
 </template>
+
+<script setup>
+function showAlert() {
+  alert("default action prevented");
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <ContentNavigation v-slot="{ navigation }">
-    <main class="bg-blue-500 flex flex-col gap-2">
+    <main class="bg-blue-500 flex flex-col gap-2 p-2">
       <div
         class="border-4 border-[#C7CFE7] rounded-lg w-fit text-lg p-1"
         v-for="link of navigation[0].children"
