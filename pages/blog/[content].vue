9169e95fc7a8e27bdbf819fd942951d2613befd7
irigyano <anhsieh10665@gmail.com>
2023-06-29 16:39:30 +0800

    feat: catch and redirect to index page

--- pages/blog/[content].vue
+++ pages/blog/[content].vue
@@ -1,30 +1,34 @@
 <template>
   <main class="max-w-full p-2">
-    <ContentDoc v-slot="{ doc }">
-      <h1 class="text-4xl font-semibold text-blue-500">{{ doc.title }}</h1>
-      <p class="text-sm opacity-30">
-        {{ convertDateString(doc.publishedAt) }}
-      </p>
-      <article class="prose-base prose-img:mx-auto">
-        <ContentRenderer :value="doc" />
-      </article>
+    <ContentDoc>
+      <template v-slot="{ doc }">
+        <h1 class="text-4xl font-semibold text-blue-500">{{ doc.title }}</h1>
+        <p class="text-sm opacity-30">
+          {{ convertDateString(doc.publishedAt) }}
+        </p>
+        <article class="prose-base prose-img:mx-auto">
+          <ContentRenderer :value="doc" />
+        </article>
+        <Giscus
+          host="https://giscus.app"
+          id="comments"
+          repo="irigyano/nuxt3-markdown-blog"
+          repoId="R_kgDOJz5Cwg"
+          category="Announcements"
+          categoryId="DIC_kwDOJz5Cws4CXi-X"
+          mapping="pathname"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme="dark"
+          lang="zh-TW"
+          async
+        />
+      </template>
+
+      <template #not-found>{{ navigateTo("/") }} </template>
     </ContentDoc>
   </main>
-  <Giscus
-    host="https://giscus.app"
-    id="comments"
-    repo="irigyano/nuxt3-markdown-blog"
-    repoId="R_kgDOJz5Cwg"
-    category="Announcements"
-    categoryId="DIC_kwDOJz5Cws4CXi-X"
-    mapping="pathname"
-    reactionsEnabled="1"
-    emitMetadata="0"
-    inputPosition="top"
-    theme="dark"
-    lang="zh-TW"
-    async
-  />
 </template>
 
 <script setup>
