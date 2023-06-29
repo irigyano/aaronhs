a6437f817a32ad90c16b1010fd0a5ca229e0e327
irigyano <anhsieh10665@gmail.com>
2023-06-29 12:14:21 +0800

    feat: add giscus at the end of the blog

--- pages/blog/[content].vue
+++ pages/blog/[content].vue
@@ -6,4 +6,24 @@
       <ContentRenderer :value="doc" />
     </ContentDoc>
   </main>
+  <Giscus
+    host="https://giscus.app"
+    id="comments"
+    repo="irigyano/nuxt3-markdown-blog"
+    repoId="R_kgDOJz5Cwg"
+    category="Announcements"
+    categoryId="DIC_kwDOJz5Cws4CXi-X"
+    mapping="pathname"
+    reactionsEnabled="1"
+    emitMetadata="0"
+    inputPosition="top"
+    theme="dark"
+    data-loading="lazy"
+    lang="zh-TW"
+    async
+  />
 </template>
+
+<script setup lang="ts">
+import Giscus from "@giscus/vue";
+</script>
