48f731c4b144b392b236358d1825e938f1b5689f
irigyano <anhsieh10665@gmail.com>
2023-07-01 14:20:21 +0800

    style: apply background color in app.vue

--- app.vue
+++ app.vue
@@ -3,3 +3,7 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+useHead({ bodyAttrs: { class: "bg-blue-300 dark:bg-red-300" } });
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="px-2 lg:px-[25vw] pb-2 bg-blue-300 dark:bg-red-300 min-h-screen font-google-fonts"
-  >
+  <div class="px-2 lg:px-[25vw] pb-2 font-google-fonts">
     <navbar />
     <slot />
   </div>
