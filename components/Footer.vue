1e2aa561d2684f9d567480be3108c91eda5dea31
irigyano <anhsieh10665@gmail.com>
2023-07-02 02:40:46 +0800

    chore: footer attribute

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
-  <footer class="text-center text-xs flex justify-around opacity-75">
+  <footer class="pt-2 text-center text-xs flex justify-around opacity-75">
     <div>
       © {{ new Date().getFullYear() }}, Built with
       <a
         class="text-green-600 font-bold hover:opacity-70 duration-300"
         href="https://nuxt.com/"
-        >Nuxt 3</a
+        >Nuxt</a
       >
     </div>
     <a
       class="text-slate-600 font-bold hover:opacity-70 duration-300"
-      href="https://github.com/irigyano/nuxt3-markdown-blog"
+      href="https://github.com/irigyano/aaronhs"
       >GitHub</a
     >
   </footer>
