42fa8d54854909a6f32facd1016c2059704cb05f
irigyano <anhsieh10665@gmail.com>
2023-07-02 01:08:09 +0800

    chore: clean up unused file

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,13 +1,7 @@
 <template>
-  <nav class="pt-4 mb-6 flex text-center">
-    <div class="basis-1/3 m-auto">
-      <NuxtLink to="/" class="text-2xl text-blue-800">Look Back</NuxtLink>
-    </div>
-    <div class="basis-1/3 m-auto">
-      <NuxtLink class="opacity-0">Categories</NuxtLink>
-    </div>
-    <div class="basis-1/3 m-auto">
-      <DarkToggle />
-    </div>
+  <nav class="pt-4 mb-6">
+    <NuxtLink to="/" class="pl-4 text-2xl font-semibold text-blue-800"
+      >Look Back</NuxtLink
+    >
   </nav>
 </template>
