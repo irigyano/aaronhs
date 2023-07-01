b0d5ae80ac6613336a262f3e0b0b7fe970bb8669
irigyano <anhsieh10665@gmail.com>
2023-07-02 01:39:35 +0800

    feat: footer

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="text-center text-xs flex justify-around opacity-75">
+    <div>
+      © {{ new Date().getFullYear() }}, Built with
+      <a class="text-green-600 font-bold" href="https://nuxt.com/">Nuxt 3</a>
+    </div>
+    <a
+      class="text-slate-600 font-bold"
+      href="https://github.com/irigyano/nuxt3-markdown-blog"
+      >GitHub</a
+    >
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="px-2 lg:px-[20vw] 2xl:px-[30vw] pb-2 font-google-fonts">
-    <navbar />
+  <div class="px-2 lg:px-[20vw] 2xl:px-[30vw] pb-4 font-google-fonts">
+    <Navbar />
     <slot />
+    <Footer />
   </div>
 </template>
