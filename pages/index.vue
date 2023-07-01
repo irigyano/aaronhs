d1c33f0e868b46b47c0f186ef7d82b5fcdba94c1
irigyano <anhsieh10665@gmail.com>
2023-07-01 13:52:47 +0800

    feat: sort blog by date

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
   <ContentNavigation v-slot="{ navigation }">
     <main class="flex flex-col gap-2 p-2">
-      <div class="p-1" v-for="link of navigation[0].children">
+      <div
+        class="p-1"
+        v-for="link of navigation[0].children.sort(
+          (blog1, blog2) =>
+            new Date(blog2.publishedAt).getTime() -
+            new Date(blog1.publishedAt).getTime()
+        )"
+      >
         <NuxtLink
           class="text-4xl font-semibold text-blue-500"
           :key="link._path"
