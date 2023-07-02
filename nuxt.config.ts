914892a000310a3df4c3cbc98281d0f417c87b3a
irigyano <anhsieh10665@gmail.com>
2023-07-03 00:34:35 +0800

    feat: show article description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   },
   content: {
     navigation: {
-      fields: ["publishedAt"],
+      fields: ["publishedAt","description"],
     },
     highlight: {
       theme: "monokai",
